de5ac92acb60d00887a52c439f0a0acfb377b8d8
jesus.gascon <jesus.gascon@gmail.com>
2021-08-13 13:28:01 +0200

    Update Ticket Test

--- src/test/java/com/prueba/springticketcrud/TicketTest.java
+++ src/test/java/com/prueba/springticketcrud/TicketTest.java
@@ -2,26 +2,227 @@ package com.prueba.springticketcrud;
 
 import com.prueba.springticketcrud.domain.Detail;
 import com.prueba.springticketcrud.domain.Ticket;
-import com.prueba.springticketcrud.repositories.TicketRepository;
+import com.prueba.springticketcrud.services.TicketService;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Set;
 
 @DataJpaTest
+@TestMethodOrder
 public class TicketTest {
 
     @Autowired
-    private TicketRepository ticketRepository;
+    private TicketService ticketService;
 
     @Test
     @DisplayName("Should Create Ticket")
-    public void shouldCreateTicket(){
-        Ticket ticket = new Ticket();
+    @Rollback(false)
+    @MethodSource("getTicketExample")
+    @Order(1)
+    public void shouldCreateTicket(Ticket ticket){
+        
+        Ticket savedTicket = ticketService.save(ticket);
+        assertNotNull(savedTicket);
+        assertFalse(savedTicket.getDetails().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should Not Create Ticket when creation Date is null")
+    @Order(2)
+    public void shouldThrowRuntimeExceptionWhenCreationDateIsNull(){
+    	Ticket ticket = new Ticket();
+        ticket.setTotalAmount(20.0);
+
+        Detail det1 = new Detail();
+        det1.setTicket(ticket);
+        det1.setDescription("Detail 1");
+        det1.setAmount(10.0);
+
+        Set<Detail> detailSet = new HashSet<>();
+        detailSet.add(det1);
+ 
+        ticket.setDetails(detailSet);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+        	ticketService.save(ticket);
+        });
+    }
+    
+    @Test
+    @DisplayName("Should Not Create Ticket when Total Amount is null")
+    @Order(3)
+    public void shouldThrowRuntimeExceptionWhenTotalAmountIsNull(){
+    	Ticket ticket = new Ticket();
+        ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
+        
+        Detail det1 = new Detail();
+        det1.setTicket(ticket);
+        det1.setDescription("Detail 1");
+        det1.setAmount(10.0);
+
+        Set<Detail> detailSet = new HashSet<>();
+        detailSet.add(det1);
+ 
+        ticket.setDetails(detailSet);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+        	ticketService.save(ticket);
+        });
+    }
+    
+    @Test
+    @DisplayName("Should Not Create Ticket when Description of Detail is null")
+    @Order(4)
+    public void shouldThrowRuntimeExceptionWhenDescriptionIsNull(){
+    	Ticket ticket = new Ticket();
+        ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
+        ticket.setTotalAmount(20.0);
+
+        Detail det1 = new Detail();
+        det1.setTicket(ticket);
+        det1.setAmount(10.0);
+
+        Set<Detail> detailSet = new HashSet<>();
+        detailSet.add(det1);
+ 
+        ticket.setDetails(detailSet);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+        	ticketService.save(ticket);
+        });
+    }
+    
+    @Test
+    @DisplayName("Should Not Create Ticket when Amount of Detail is null")
+    @Order(5)
+    public void shouldThrowRuntimeExceptionWhenAmountIsNull(){
+    	Ticket ticket = new Ticket();
+        ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
+        ticket.setTotalAmount(20.0);
+
+        Detail det1 = new Detail();
+        det1.setTicket(ticket);
+        det1.setDescription("Detail 1");
+
+        Set<Detail> detailSet = new HashSet<>();
+        detailSet.add(det1);
+ 
+        ticket.setDetails(detailSet);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+        	ticketService.save(ticket);
+        });
+    }
+    
+    @Test
+    @DisplayName("Should Find By Id")
+    @Order(6)
+    public void shouldFindById(Ticket ticket) {
+        
+        Ticket findTicket = ticketService.findById(1L);
+        
+        assertNotNull(findTicket);
+    }
+    
+    @Test
+    @DisplayName("Should Find All Tickets")
+    @Order(7)
+    public void shouldFindAllTickets() {
+        
+        Set<Ticket> findTickets = ticketService.findAll();
+        
+        assertFalse(findTickets.isEmpty());
+    }
+    
+    @Test
+    @DisplayName("Should Find By Creation Date Between")
+    @Order(8)
+    public void shouldFindByCreationDateBetween() {
+        
+    	Calendar startDate = new GregorianCalendar();
+    	startDate.add(Calendar.HOUR, -1);
+    	Calendar endDate = new GregorianCalendar();
+    	endDate.add(Calendar.HOUR, 1);
+    	
+        Set<Ticket> findTickets = ticketService.findByCreationDateBetween(new Timestamp(startDate.getTimeInMillis()), 
+        															new Timestamp(endDate.getTimeInMillis()));
+        
+        assertFalse(findTickets.isEmpty());
+    }
+    
+    @Test
+    @DisplayName("Should Not Find By Creation Date Between")
+    @Order(9)
+    public void shouldFindByCreationDateBetween() {
+        
+    	Calendar startDate = new GregorianCalendar();
+    	startDate.add(Calendar.HOUR, 1);
+    	Calendar endDate = new GregorianCalendar();
+    	endDate.add(Calendar.HOUR, 2);
+    	
+        Set<Ticket> findTickets = ticketService.findByCreationDateBetween(new Timestamp(startDate.getTimeInMillis()), 
+        															new Timestamp(endDate.getTimeInMillis()));
+        
+        assertTrue(findTickets.isEmpty());
+    }
+    
+    @Test
+    @DisplayName("Should Update Ticket")
+    @Order(10)
+    @Rollback(false)
+    public void shouldUpdateTicket() {
+    	
+    	Ticket findTicket = ticketService.findById(1L);
+        Double newAmount = 30.0;
+        findTicket.setTotalAmount(newAmount);
+        ticketService.save(ticket);
+        
+        Ticket updatedTicket = ticketService.findById(1L);
+        		
+        assertThat(updatedTicket.getTotalAmount()).isEqualTo(newAmount);
+    }
+    
+    @Test
+    @DisplayName("Should Delete Ticket by Id")
+    @Rollback(false)
+    @Order(11)
+    public void shouldDeleteTicketById() {
+    	
+    	boolean existBefore = ticketService.findById(1L).isPresent();
+    	ticketService.deleteById(1);
+    	boolean existAfter = ticketService.findById(1L).isPresent();
+    	
+    	assertTrue(existBefore);
+    	assertFalse(existAfter);
+    }
+    
+    @Test
+    @DisplayName("Should Delete Ticket")
+    @Rollback(false)
+    @MethodSource("getTicketExample")
+    @Order(12)
+    public void shouldDeleteTicket(Ticket ticket) {
+    	
+    	Ticket savedTicket = ticketService.save(ticket);
+    	boolean existBefore = ticketService.findById(savedTicket.getId()).isPresent();
+    	ticketService.delete(savedTicket);
+    	boolean existAfter = ticketService.findById(savedTicket.getId()).isPresent();
+    	
+    	assertTrue(existBefore);
+    	assertFalse(existAfter);
+    	
+    }
+    
+    private Ticket getTicketExample() {
+    	Ticket ticket = new Ticket();
         ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
         ticket.setTotalAmount(20.0);
 
@@ -46,92 +247,6 @@ public class TicketTest {
         detailSet.add(det3);
 
         ticket.setDetails(detailSet);
-
-        ticketRepository.save(ticket);
-        
-    }
-
-    @Test
-    @DisplayName("Should Not Create Ticket when creation Date is null")
-    public void shouldThrowRuntimeExceptionWhenCreationDateIsNull(){
-    	Ticket ticket = new Ticket();
-        ticket.setTotalAmount(20.0);
-
-        Detail det1 = new Detail();
-        det1.setTicket(ticket);
-        det1.setDescription("Detail 1");
-        det1.setAmount(10.0);
-
-        Set<Detail> detailSet = new HashSet<>();
-        detailSet.add(det1);
- 
-        ticket.setDetails(detailSet);
-
-        Assertions.assertThrows(RuntimeException.class, () -> {
-        	ticketRepository.save(ticket);
-        });
     }
     
-    @Test
-    @DisplayName("Should Not Create Ticket when Total Amount is null")
-    public void shouldThrowRuntimeExceptionWhenTotalAmountIsNull(){
-    	Ticket ticket = new Ticket();
-        ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
-        
-        Detail det1 = new Detail();
-        det1.setTicket(ticket);
-        det1.setDescription("Detail 1");
-        det1.setAmount(10.0);
-
-        Set<Detail> detailSet = new HashSet<>();
-        detailSet.add(det1);
- 
-        ticket.setDetails(detailSet);
-
-        Assertions.assertThrows(RuntimeException.class, () -> {
-        	ticketRepository.save(ticket);
-        });
-    }
-    
-    @Test
-    @DisplayName("Should Not Create Ticket when Description of Detail is null")
-    public void shouldThrowRuntimeExceptionWhenDescriptionIsNull(){
-    	Ticket ticket = new Ticket();
-        ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
-        ticket.setTotalAmount(20.0);
-
-        Detail det1 = new Detail();
-        det1.setTicket(ticket);
-        det1.setAmount(10.0);
-
-        Set<Detail> detailSet = new HashSet<>();
-        detailSet.add(det1);
- 
-        ticket.setDetails(detailSet);
-
-        Assertions.assertThrows(RuntimeException.class, () -> {
-        	ticketRepository.save(ticket);
-        });
-    }
-    
-    @Test
-    @DisplayName("Should Not Create Ticket when Amount of Detail is null")
-    public void shouldThrowRuntimeExceptionWhenAmountIsNull(){
-    	Ticket ticket = new Ticket();
-        ticket.setCreationDate(new Timestamp(System.currentTimeMillis()));
-        ticket.setTotalAmount(20.0);
-
-        Detail det1 = new Detail();
-        det1.setTicket(ticket);
-        det1.setDescription("Detail 1");
-
-        Set<Detail> detailSet = new HashSet<>();
-        detailSet.add(det1);
- 
-        ticket.setDetails(detailSet);
-
-        Assertions.assertThrows(RuntimeException.class, () -> {
-        	ticketRepository.save(ticket);
-        });
-    }
 }
