7f44ab9f7b57d48cdefafd1636098eb27823d5f3
Jesus Gascon <jgasconrueda@gmail.com>
2022-01-11 19:22:05 +0100

    sleuth and zipkin

--- src/main/java/com/prueba/springticketcrud/services/TicketServiceImpl.java
+++ src/main/java/com/prueba/springticketcrud/services/TicketServiceImpl.java
@@ -3,6 +3,8 @@ package com.prueba.springticketcrud.services;
 import com.prueba.springticketcrud.domain.Ticket;
 import com.prueba.springticketcrud.exceptions.TicketNotFoundException;
 import com.prueba.springticketcrud.repositories.TicketRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ import java.util.Optional;
 public class TicketServiceImpl implements TicketService {
 
     private final TicketRepository ticketRepository;
+    private Logger logger = LoggerFactory.getLogger(TicketServiceImpl.class);
 
     public TicketServiceImpl(TicketRepository ticketRepository) {
         this.ticketRepository = ticketRepository;
@@ -24,24 +27,29 @@ public class TicketServiceImpl implements TicketService {
     public List<Ticket> findAll() {
         List<Ticket> tickets = new ArrayList<>();
         ticketRepository.findAll().forEach(tickets::add);
+        logger.info("TicketServiceImpl - findAll: Return "+tickets.size()+" tickets");
         return tickets;
     }
 
     @Override
     public Optional<Ticket> findById(Long aLong) {
+        logger.info("TicketServiceImpl - findById: Ticket id "+ aLong);
         return ticketRepository.findById(aLong);
     }
 
     @Override
     public Ticket save(Ticket ticket) {
+        logger.info("TicketServiceImpl - save: "+ticket.toString());
         return ticketRepository.save(ticket);
     }
 
     @Override
     public Ticket update(Long aLong, Ticket object) {
+        logger.info("TicketServiceImpl - update: TicketId "+aLong);
         Optional<Ticket> ticket = ticketRepository.findById(aLong);
 
         if(!ticket.isPresent()){
+            logger.error("TicketServiceImpl - update: TicketNotFound");
             throw new TicketNotFoundException("Ticket with id "+aLong+" not found!!");
         }
 
@@ -54,11 +62,13 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public void delete(Ticket object) {
+        logger.info("TicketServiceImpl - delete: "+object.toString());
         ticketRepository.delete(object);
     }
 
     @Override
     public void deleteById(Long aLong) {
+        logger.info("TicketServiceImpl - deleteById: TicketId "+aLong);
         ticketRepository.deleteById(aLong);
     }
 
